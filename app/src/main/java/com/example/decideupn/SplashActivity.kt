13b30539a90b6dd4df32490600f34fd90fe60eba
JohanPorras04 <johanaldairporraspereda@gmail.com>
2025-05-06 12:13:17 -0500

    subiendo cosas

---
+++ app/src/main/java/com/example/decideupn/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.decideupn
+
+import android.os.Bundle
+import android.content.Intent
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.os.Handler
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main2)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish() // Cierra SplashActivity
+        }, 2000)
+    }
+}
